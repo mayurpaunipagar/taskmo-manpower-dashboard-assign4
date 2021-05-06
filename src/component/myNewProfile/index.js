fd17000b50f9f851095977f6d55a5bbf1f24c9f4
DESKTOP-UCFV2QI\info <abhijit.mohanty@feedmypockets.com>
2021-05-06 13:07:35 +0530

    myNewProfile Style updated and code optimised

--- src/component/myNewProfile/index.js
+++ src/component/myNewProfile/index.js
@@ -2,6 +2,17 @@ import "./style.css";
 import addProfileIcon from "./../../icons/add-profile.png";
 
 export default function MyNewProfile() {
+  const formData = [
+    "Name",
+    "Email",
+    "Contact",
+    "Whatsapp",
+    "Job Role",
+    "State",
+    "City",
+    "Pincode",
+  ];
+
   return (
     <>
       <div className="new-profile-container">
@@ -16,54 +27,33 @@ export default function MyNewProfile() {
               />
             </div>
             <div className="my-form">
-              <label className="profile-input-label">Name</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">Email</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">Contact</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">Whatsapp</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">Job Role</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">State</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">City</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
-              <label className="profile-input-label">Pincode</label>
-              <input
-                className="profile-input-field"
-                type="text"
-                placeholder="Eg. your text here"
-              />
+              {formData.map((el) => {
+                if (el === "Job Role" || el === "State" || el === "City") {
+                  return (
+                    <>
+                      <label className="profile-input-label">{el}</label>
+                      <select
+                        className="profile-input-field gray"
+                        // type="text"
+                        // placeholder="Eg. your text here"
+                      >
+                        <option value="Eg. your text here">Eg. your text here</option>
+                        </select>
+                    </>
+                  );
+                } else {
+                  return (
+                    <>
+                      <label className="profile-input-label">{el}</label>
+                      <input
+                        className="profile-input-field"
+                        type="text"
+                        placeholder="Eg. your text here"
+                      />
+                    </>
+                  );
+                }
+              })}
               <div className="profile-button-container">
                 <button className="profile-btn-add btn-style">ADD</button>
                 <button className="profile-btn-clear btn-style">CLEAR</button>
